c1c7e046d06cc9dad30a38bf4b5287afccfb563f
brend3n <brenden@knights.ucf.edu>
2022-08-15 20:44:29 -0400

    Added classes as well as exponential backoff

--- script.py
+++ script.py
@@ -1,15 +1,26 @@
+# Youtube->Mp3 downloader
 from pytube import YouTube
+
+# File handling
 import os
 import csv
+
+# Webscraping
 import requests
 from bs4 import BeautifulSoup
 from Web_Scrape_Tool.web_scrape_tool import get_soup_adv
-from alive_progress import alive_bar # Progress bar
-import threading # Multithreading for faster scanning
 
+# CLI
+from alive_progress import alive_bar # Progress bar
+
+# Threading and Utilities
+import threading # Multithreading for faster scanning
+import random
 import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from time import sleep
+
+# Disable warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 """
 GET https://convert2mp3s.com/api/button/{FTYPE}?url={VIDEO_URL}
@@ -27,92 +38,124 @@ SPOTIFY_ID  = 'SpotifyID'
 IRSC        = 'ISRC'
 
 RATE_LIMIT_SLEEP_TIMEOUT = 20 # 20 second delay
-# I copied most of this code from somewhere else
-def download(url_: str, file_name_: str):
-    if(url_ == ""):
-        return
-    # url input from user
-    yt = YouTube(url_)
+
+class DownloaderThread(threading.Thread):
+    backoff_time = 0
+    minimum_backoff_time = 1 #second
+    MAC_BACKOFF_TIME = 60 #seconds
     
-    # extract only audio
-    video = yt.streams.filter(only_audio=True).first()
 
-    destination = file_name_ if file_name_ != "" else "./DownloadedSongs/"
-    
-    # download the file
-    out_file = video.download(output_path=destination)
-    
-    # save the file
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    
-    # result of success
-    print(yt.title + " has been successfully downloaded.")
+    def __init__(self, chunk, file_name, bar) -> None:
+        threading.Thread.__init__(self)
+        self.chunk = chunk
+        self.file_name = file_name
+        self.bar = bar
 
-# TODO: Find the most viewed song
-def get_most_viewed_song(links):
+    def run(self) -> None:
+        self.do_process()
 
-    if (len(links) > 0):
-        return links[0]
-    else:
-        return
+    def backoff(self):
+        if(self.minimum_backoff_time > self.MAC_BACKOFF_TIME):
+            return
+        delay = self.minimum_backoff_time + random.randint(0,1000) / 1000.0
+        print(f"Current backoff: {delay}")
+        sleep(delay)
+        self.minimum_backoff_time *= 2
 
-def search_for_song(song_string: str):
-    youtube_url = ""
-    youtube_links = []
-
-    print(f"Searching for: {song_string}")
-
-    # Creating search string
-    base_search_string = f"https://www.google.com/search?q=site:youtube.com+{song_string}"
-
-    # Making request
-    soup = get_soup_adv(base_search_string)
-
-    # check if soup had bad response status code
-    if (soup is None):
-        # wait a bit before trying again
-        sleep(RATE_LIMIT_SLEEP_TIMEOUT)
-        return None
-
-    links = soup.find_all('a', href=True)
-
-    # Getting all youtube links
-    for youtube_link in links:
-        href = youtube_link['href'] # get links
-        if "https://www.youtube.com/watch?" not in href: # Not a youtube link
-            continue
-        else:
-            youtube_links.append(href)
-
-    # Remove duplicates
-    youtube_links = list(set(youtube_links))
-
-    youtube_url = get_most_viewed_song(youtube_links)
-
-    return youtube_url
-
-# Gaurantees a not None URL
-def retrieve_song(song):
-    url = ""
-    res = False
-
-    while(res == False):
-        try:
-            url = search_for_song(song)
-            if (url is None):
-                raise Exception
-        except Exception:
-            continue
+    # I copied most of this code from somewhere else
+    def download(url_: str, file_name_: str):
+        if(url_ == ""):
+            return
+        # url input from user
+        yt = YouTube(url_)
         
-        if url is None:
-            continue
+        # extract only audio
+        video = yt.streams.filter(only_audio=True).first()
+
+        destination = file_name_ if file_name_ != "" else "./DownloadedSongs/"
+        
+        # download the file
+        out_file = video.download(output_path=destination)
+        
+        # save the file
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        
+        # result of success
+        print(yt.title + " has been successfully downloaded.")
+
+    # TODO: Find the most viewed song
+    def get_most_viewed_song(links):
+
+        if (len(links) > 0):
+            return links[0]
         else:
-            res = True
+            return
 
-    return url
+    def search_for_song(self, song_string: str):
+        youtube_url = ""
+        youtube_links = []
 
+        print(f"Searching for: {song_string}")
+
+        # Creating search string
+        base_search_string = f"https://www.google.com/search?q=site:youtube.com+{song_string}"
+
+        # Making request
+        soup = get_soup_adv(base_search_string)
+
+        # check if soup had bad response status code
+        if (soup is None):
+            # wait a bit before trying again
+            self.backoff()
+            return None
+
+        links = soup.find_all('a', href=True)
+
+        # Getting all youtube links
+        for youtube_link in links:
+            href = youtube_link['href'] # get links
+            if "https://www.youtube.com/watch?" not in href: # Not a youtube link
+                continue
+            else:
+                youtube_links.append(href)
+
+        # Remove duplicates
+        youtube_links = list(set(youtube_links))
+
+        youtube_url = self.get_most_viewed_song(youtube_links)
+
+        return youtube_url
+
+    # Gaurantees a not None URL
+    def retrieve_song(self, song):
+        url = ""
+        res = False
+
+        while(res == False):
+            try:
+                url = self.search_for_song(song)
+                if (url is None):
+                    raise Exception
+            except Exception:
+                continue
+            
+            if url is None:
+                continue
+            else:
+                res = True
+
+        return url
+
+    def do_process(self):
+        for song in self.chunk:
+            url = self.retrieve_song(song)
+            self.download(url, self.file_name)
+            sleep(5) # rate limit
+            self.bar()
+
+# Convert Songs to dictionary from CSV file
 def read_from_csv(file_name: str):
     list_of_strings = []
     with open(file_name, 'r') as file:
@@ -126,11 +169,11 @@ def read_from_csv(file_name: str):
             list_of_strings.append(string)
     return list_of_strings
 
-# Used for making chunks
+# Partitioning data for each thread
 def chunkify(lst,n):
     return [lst[i::n] for i in range(n)]
 
-# Use threads to speed up scanning
+# Start the threads
 def launch_threads(prog_bar_obj, num_threads, all_links, file_name):
     
     # Divide chunks of webpages to each thread
@@ -141,7 +184,7 @@ def launch_threads(prog_bar_obj, num_threads, all_links, file_name):
     
     # Give each thread webpages
     for i in range(num_threads):
-        t = threading.Thread(name=f"Thread {i}", target=do_process, args=(chunks[i],file_name,prog_bar_obj,) )
+        t = DownloaderThread(chunks[i], file_name, prog_bar_obj)
         t.setDaemon(True)
         threads.append(t)
     
@@ -155,13 +198,6 @@ def launch_threads(prog_bar_obj, num_threads, all_links, file_name):
     for i in range(num_threads):
         threads[i].join()
 
-def do_process(chunk_of_songs, file_name, bar):
-    for song in chunk_of_songs:
-        url = retrieve_song(song)
-        download(url,file_name)
-        sleep(5) # rate limit
-        bar()
-
 def main():
     
     choice = int(input("1. Enter URL\n2. Find single song\n3. Read from file\n"))
@@ -169,11 +205,13 @@ def main():
     if (choice == 1):
         # Enter filetype somewhere else, maybe use simple term
         url = input("Enter a url: ")
-        download(song, file_name)
+        t_thread = DownloaderThread()
+        t_thread.download(song, file_name)
     elif (choice == 2):
         song = input("Enter song: ")
-        url = retrieve_song(song)
-        download(url,file_name)
+        t_thread = DownloaderThread()
+        url = t_thread.retrieve_song(song)
+        t_thread.download(url,file_name)
     elif (choice == 3):
         num_threads = int(input("Enter number of threads to use: "))
         # file_name = input("BRENDEN enter CSV instructions at some point")
